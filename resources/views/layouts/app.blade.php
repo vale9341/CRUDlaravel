764f3a7aeb650c9c39ab0acc7328e43bf76ca2cd
Jose Valentin Angulo Sanchez <vale9341@gmail.com>
2018-12-21 01:20:11 -0600

    tirth commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,6 @@
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
-                    HOLA
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
